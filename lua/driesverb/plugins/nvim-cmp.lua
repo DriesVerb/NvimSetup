77ab6b5f6fa02bf67255a5122ab53744ce4ed580
Dries Verboven <verboven.dries@gmail.com>
2022-12-30 15:03:16 +0100

    adding lsp saga

--- lua/driesverb/plugins/nvim-cmp.lua
+++ lua/driesverb/plugins/nvim-cmp.lua
@@ -10,6 +10,12 @@ if not luasnip_status then
   return
 end
 
+-- import lspkind plugin safely
+local lspkind_status, lspkind = pcall(require, "lspkind")
+if not lspkind_status then
+  return
+end
+
 -- load vs-code like snippets from plugins (e.g. friendly-snippets)
 require("luasnip/loaders/from_vscode").lazy_load()
 
@@ -21,6 +27,8 @@ cmp.setup({
       luasnip.lsp_expand(args.body)
     end,
   },
+
+  -- keymaps 
   mapping = cmp.mapping.preset.insert({
     ["<C-k>"] = cmp.mapping.select_prev_item(), -- previous suggestion
     ["<C-j>"] = cmp.mapping.select_next_item(), -- next suggestion
@@ -30,6 +38,7 @@ cmp.setup({
     ["<C-e>"] = cmp.mapping.abort(), -- close completion window
     ["<CR>"] = cmp.mapping.confirm({ select = false }),
   }),
+
   -- sources for autocompletion
   sources = cmp.config.sources({
     { name = "nvim_lsp" }, -- lsp
@@ -37,4 +46,12 @@ cmp.setup({
     { name = "buffer" }, -- text within current buffer
     { name = "path" }, -- file system paths
   }),
+
+   -- configure lspkind for vs-code like icons
+  formatting = {
+    format = lspkind.cmp_format({
+      maxwidth = 50,
+      ellipsis_char = "...",
+    }),
+  },
 })
