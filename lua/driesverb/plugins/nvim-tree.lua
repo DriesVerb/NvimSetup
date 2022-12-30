b2164e2d035caf05ab8fa8859a76b35d41099691
Dries Verboven <verboven.dries@gmail.com>
2022-12-30 11:28:16 +0100

    adding a better formatting for nvim-tree

--- lua/driesverb/plugins/nvim-tree.lua
+++ lua/driesverb/plugins/nvim-tree.lua
@@ -8,8 +8,33 @@ end
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
 
--- set termguicolors to enable highlight groups
-vim.opt.termguicolors = true
+-- change color for arrows in tree to light blue
+vim.cmd([[ highlight NvimTreeIndentMarker guifg=#3FC5FF ]])
 
--- empty setup using defaults
-require("nvim-tree").setup()
+-- configure nvim-tree
+nvimtree.setup({
+  -- change folder arrow icons
+  renderer = {
+    icons = {
+      glyphs = {
+        folder = {
+          arrow_closed = "", -- arrow when folder is closed
+          arrow_open = "", -- arrow when folder is open
+        },
+      },
+    },
+  },
+  -- disable window_picker for
+  -- explorer to work well with
+  -- window splits
+  actions = {
+    open_file = {
+      window_picker = {
+        enable = false,
+      },
+    },
+  },
+  	git = {
+  		ignore = false,
+  	},
+})
