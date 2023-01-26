10677156e81220a51e93868feb8405ed43284dbf
Dries Verboven <verboven.dries@gmail.com>
2023-01-26 19:57:18 +0100

    adding new modules, but put in ignor for yaml

--- lua/driesverb/plugins/tree-sitter.lua
+++ lua/driesverb/plugins/tree-sitter.lua
@@ -3,7 +3,7 @@ require("nvim-treesitter.configs").setup({
 
 	sync_install = false,
 	auto_install = true,
-	-- ignore_install = { "javascript" },
+	ignore_install = { "yaml" },
 
 	highlight = {
 		enable = true,
