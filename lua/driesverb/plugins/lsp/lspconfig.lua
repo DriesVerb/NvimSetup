464112bc8dc220c51e1e6c809170c1b250a8d5c3
Dries Verboven <verboven.dries@gmail.com>
2023-03-14 14:54:50 +0100

    adding better diagnostics

--- lua/driesverb/plugins/lsp/lspconfig.lua
+++ lua/driesverb/plugins/lsp/lspconfig.lua
@@ -70,4 +70,17 @@ typescript.setup({
 	},
 })
 
-vim.diagnostic.config()
+vim.diagnostic.config({
+	virtual_text = false, -- disable virtual text
+	update_in_insert = true,
+	underline = true,
+	severity_sort = true,
+	float = {
+		focusable = true,
+		style = "minimal",
+		border = "rounded",
+		source = "always",
+		header = "",
+		prefix = "",
+	},
+})
